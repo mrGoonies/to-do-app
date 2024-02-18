cba09a8e26499e122b0943aacbf6f4db8d759d5f
mrGoonies <gooniesdev@gmail.com>
2024-02-17 22:40:19 -0300

    Add: Document endpoints

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" main project file """
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,10 +7,20 @@ tasks = list()
 
 @app.get("/")
 def index() -> dict[str, str]:
+    """ Display a message to test that everything is correct
+
+    Returns:
+        dict[str, str]: Dictionary with values ​​(key and value) of type strings
+    """
     return {"message": "Hello, World"}
 
 @app.get("/task")
-def get_all_task() -> dict:
+def get_all_task() -> list:
+    """list all stored tasks
+
+    Returns:
+        list: list of stored tasks
+    """
     if len(tasks) == 0:
         return {"Message": "No task available"}
-    return {"Task": tasks}
+    return tasks
